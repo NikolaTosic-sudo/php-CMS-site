6d4e41b07c9e114194abac9e71cae326348882ea
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-07-24 15:15:05 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -3,11 +3,12 @@
 $db['db_host'] = 'localhost';
 $db['db_user'] = 'root';
 $db['db_name'] = 'cms';
+$db['db_password'] = '';
 
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
 }
 
-$connection = mysqli_connect(DB_HOST, DB_USER, '', DB_NAME);
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 
