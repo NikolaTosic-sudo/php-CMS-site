0c272c5e52cdfd3d860b1050fb2add0b02643c9e
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-07 14:56:02 +0200

    Update functions.php
    Adjusting code to use prepared statements and making new function because code got too long for a page

--- admin/functions.php
+++ admin/functions.php
@@ -192,4 +192,132 @@ function alreadyExists($row, $object) {
 }
 
 
+function showAllPosts($username, $limit) {
+
+    global $connection;
+
+    if ($username == null){
+
+        $stmt = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts WHERE post_status = ?");
+
+        $published = 'published';
+
+        mysqli_stmt_bind_param($stmt, 's', $published);
+
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_store_result($stmt);
+
+        $count = mysqli_stmt_num_rows($stmt);
+
+        if($count < 1) {
+
+            echo "<h2 class='text-center text-danger'>NO POSTS AVAILABLE</h2>";
+
+        } else {
+
+            $count = ceil($count / 5);
+
+            $stmt = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts WHERE post_status = ? LIMIT $limit, 5");
+
+            mysqli_stmt_bind_param($stmt, 's', $published);
+
+            mysqli_stmt_execute($stmt);
+
+        }
+
+    }
+
+    else if (is_admin($username)) {
+
+        $stmt = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts");
+
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_store_result($stmt);
+
+        $count = mysqli_stmt_num_rows($stmt);
+
+        if($count < 1) {
+
+            echo "<h2 class='text-center text-danger'>NO POSTS AVAILABLE</h2>";
+
+        } else {
+
+            $count = ceil($count / 5);
+
+            $stmt = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts LIMIT $limit, 5");
+
+            mysqli_stmt_execute($stmt);
+
+        }
+
+    } else {
+
+        $stmt = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts WHERE post_status = ?");
+
+        $published = 'published';
+
+        mysqli_stmt_bind_param($stmt, 's', $published);
+
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_store_result($stmt);
+
+        $count = mysqli_stmt_num_rows($stmt);
+
+        if($count < 1) {
+
+            echo "<h2 class='text-center text-danger'>NO POSTS AVAILABLE</h2>";
+
+        } else {
+
+            $count = ceil($count / 5);
+
+            $stmt = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content, post_status FROM posts WHERE post_status = ? LIMIT $limit, 5");
+
+            mysqli_stmt_bind_param($stmt, 's', $published);
+
+            mysqli_stmt_execute($stmt);
+
+        }
+
+    }
+
+        mysqli_stmt_bind_result($stmt, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content, $post_status);
+
+        while(mysqli_stmt_fetch($stmt)) {
+
+            ?>
+
+
+            <h2>
+                <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="author_post.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id ?>"><?php echo $post_author ?></a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+            <hr>
+            <a href="post.php?p_id=<?php echo $post_id ?>">
+                <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
+            </a>
+            <hr>
+            <p><?php echo substr($post_content,0,100)?></p>
+            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+            <hr>
+
+
+            <?php
+
+
+
+}
+
+return $count;
+
+}
+
+
 
