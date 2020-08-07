e837d6ee31a25709681aff533742e3e00cb371a3
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-07 14:56:59 +0200

    Update index.php
    User showAllPosts function

--- index.php
+++ index.php
@@ -38,74 +38,8 @@
 
                 }
 
-                if ($_SESSION['username'] == null){
-
-                    $post_count_query = "SELECT * FROM posts WHERE post_status = 'published'";
-
-                }
-
-                else if (is_admin($_SESSION['username'])) {
-
-                    $post_count_query = "SELECT * FROM posts ";
-
-                } else {
-
-                    $post_count_query = "SELECT * FROM posts WHERE post_status = 'published'";
-
-                }
-
-                $find_count = mysqli_query($connection, $post_count_query);
-
-                $count = mysqli_num_rows($find_count);
-
-
-                if($count < 1) {
-
-                    echo "<h2 class='text-center text-danger'>NO POSTS AVAILABLE</h2>";
-
-                } else {
-
-                $count = ceil($count / 5);
-
-                $post_count_query .=  " LIMIT $page_1, 5";
-
-                $select_all_posts = mysqli_query($connection, $post_count_query);
-
-                while($row = mysqli_fetch_assoc($select_all_posts)) {
-                    $post_id = $row['post_id'];
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = substr($row['post_content'],0,100);
-                    $post_status = $row['post_status'];
-
-                   ?>
-
-
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="author_post.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id ?>"><?php echo $post_author ?></a>
-                </p>
-                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-                <hr>
-                    <a href="post.php?p_id=<?php echo $post_id ?>">
-                        <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
-                    </a>
-                <hr>
-                <p><?php echo $post_content?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-
-                <?php
-
-
-                    }}
-
+                $count = showAllPosts($_SESSION['username'], $page_1);
+                
 
                 ?>
 
