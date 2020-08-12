c2a45266e2e635c2c96e7bd82b1ddcb3ff43ae6f
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-12 18:33:38 +0200

    Update sidebar.php
    Changing sidebar to show something only when user is logged in/out

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,21 +1,19 @@
 <div class="col-md-4">
 
-    <!-- Blog Search Well -->
-    <div class="well">
-        <h4>Blog Search</h4>
 
-        <form action="./search.php" method="post">
-            <div class="input-group">
-                <input type="text" name="search" class="form-control">
-                <span class="input-group-btn">
-                                <button name="submit" class="btn btn-default" type="submit">
-                                    <span class="glyphicon glyphicon-search"></span>
-                                </button>
-                </span>
-            </div>
-        </form>
-        <!-- /.input-group -->
-    </div>
+    <?php
+
+    if (isset($_SESSION['username'])){
+
+        include "addpostWell.php" ;
+
+    }
+
+    ?>
+
+
+    <!-- Blog Search Well -->
+    <?php  include "searchbar.php" ?>
 
     <!--  Login  -->
 
@@ -98,6 +96,18 @@
     </div>
 
     <!-- Side Widget Well -->
-    <?php include "widget.php" ?>
+
+
+
+    <?php
+
+    if (!isset($_SESSION['username'])){
+
+        include "widget.php" ;
+
+    }
+
+
+    ?>
 
 </div>
